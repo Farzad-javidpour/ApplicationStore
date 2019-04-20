de5fcb43feca987e24edef5df50011d2f2d50687
Farzad <javidpour.info@gmail.com>
2019-04-20 12:28:28 +0430

    رفع مشکلات مربوط به لاگین کاربران معمولی پس از رجیستر

--- ApplicationStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ApplicationStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -112,23 +112,28 @@ namespace ApplicationStore.Areas.Identity.Pages.Account
                     //-----------------------------------------------------------
 
 
-                    _logger.LogInformation("User created a new account with password.");
+                    //_logger.LogInformation("User created a new account with password.");
 
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(applicationStoreUser);
+                    if (!User.IsInRole(StaticInfo.SuperAdminUser))
+                    {
+                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(applicationStoreUser);
 
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { userId = applicationStoreUser.Id, code = code },
-                    //    protocol: Request.Scheme);
+                        var callbackUrl = Url.Page(
+                            "/Account/ConfirmEmail",
+                            pageHandler: null,
+                            values: new { userId = applicationStoreUser.Id, code = code },
+                            protocol: Request.Scheme);
 
-                    ////await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    ////    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    //await _signInManager.SignInAsync(applicationStoreUser, isPersistent: false);
-                    //return LocalRedirect(returnUrl);
-
-                    return RedirectToAction("Index", "AdminUsers", new { area = "Admin" }); // Added by developer
+                        await _signInManager.SignInAsync(applicationStoreUser, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "AdminUsers", new { area = "Admin" }); // Added by developer
+                    }
                 }
                 foreach (var error in result.Errors)
                 {
