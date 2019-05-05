9f98f802cf4af4ffb642b00160eccb8e865d4818
mojtaba jafari <mojtabajafariiust@gmail.com>
2019-05-05 16:36:01 +0430

    Add Nlog and create a class to log

---
+++ ApplicationStore/MyLogger.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationStore
+{
+    public static class MyLogger
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Log(string message)
+        {
+            logger.Info(message);
+        }
+    }
+}
--- ApplicationStore/Startup.cs
+++ ApplicationStore/Startup.cs
@@ -29,6 +29,7 @@ namespace ApplicationStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            MyLogger.Log("ApplicationStore Started.");
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
